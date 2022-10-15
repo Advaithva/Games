d45e1250edaac158e557b7ea2b30ed2c93d77ee2
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-10-15 07:43:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function A() {
+  alert("Use Arrow keys to move the jetpack girl. Avoid touching the 2 dragons. Using the arrow keys touch the green line and score points.")
+}
+
+function B() {
+  alert("")
+}
+
+function C() {
+  alert("")
+}
+
+function D() {
+  alert("")
+}
