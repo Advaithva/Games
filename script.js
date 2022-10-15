b31608007f9a075df67b910aafd518dcfa67b186
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-10-15 07:47:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function A() {
-  alert("Use Arrow keys to move the jetpack girl. Avoid touching the 2 dragons. Using the arrow keys touch the green line and score points.")
+  alert("Click on the green flag. Use Arrow keys to move the jetpack girl. Avoid touching the 2 dragons. Using the arrow keys touch the green line and score points.")
 }
 
 function B() {
-  alert("Follow the instructions correctly. Play maze to get money. In maze reach the finishing point without touching the red lines within 20 seconds. Spend money by watching a movie or eating in a restaurant.")
+  alert("Click on the green flag. Follow the instructions correctly. Play maze to get money. In maze reach the finishing point without touching the red lines within 20 seconds. Spend money by watching a movie or eating in a restaurant.")
 }
 
 function C() {
